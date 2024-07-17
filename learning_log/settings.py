a1778748faef02ad88f3f359f621ff56eb8db351
donald <donaldwitherspoon21@yahoo.com>
2024-07-17 03:48:24 -0400

    Set DEBUG based on environment variables.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -140,5 +140,10 @@ import django_heroku
 django_heroku.settings(locals())
 
 
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
+
 
 
